f1e772bb2343ba4a4a5e51c1ffbda48870c79edf
rouwenee <rubyannad@gmail.com>
2023-10-27 13:58:05 -0500

    fourth commit

--- app/src/main/java/com/cs407/journeydoodle/MainActivity.java
+++ app/src/main/java/com/cs407/journeydoodle/MainActivity.java
@@ -1,8 +1,13 @@
 package com.cs407.journeydoodle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+
+            }
+        });
+
     }
 }

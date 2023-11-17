4b5c94c0dbce5d05c84a4b2f0b6e0ef67f06bfdc
rouwenee <rubyannad@gmail.com>
2023-11-17 13:06:11 -0600

    saved routes working

--- app/src/main/java/com/cs407/journeydoodle/savedRoutes.java
+++ app/src/main/java/com/cs407/journeydoodle/savedRoutes.java
@@ -2,11 +2,18 @@ package com.cs407.journeydoodle;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class savedRoutes extends AppCompatActivity {
+    public void exit(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

9e414265a18eb8bc342f4ac42af6fcb750166658
rouwenee <rubyannad@gmail.com>
2023-11-17 13:42:07 -0600

    Save button working

--- app/src/main/java/com/cs407/journeydoodle/SaveDialog.java
+++ app/src/main/java/com/cs407/journeydoodle/SaveDialog.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,13 +14,16 @@ public class SaveDialog extends AppCompatDialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        final EditText taskEditText = new EditText(getContext().getApplicationContext());
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle("Save Route")
                 .setMessage("Please Enter the Route Name")
+                .setView(taskEditText)
+                .setNegativeButton("Cancel", null)
                 .setPositiveButton("SAVE", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-
+                        String task = String.valueOf(taskEditText.getText());
                     }
                 });
         return builder.create();

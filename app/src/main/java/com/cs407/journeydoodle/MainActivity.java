4aa9039047dac70680b3d6702d9cab9202737a9c
Louis Digon <digon@wisc.edu>
2023-12-01 13:12:59 -0600

    Adding identifier class for login page

--- app/src/main/java/com/cs407/journeydoodle/MainActivity.java
+++ app/src/main/java/com/cs407/journeydoodle/MainActivity.java
@@ -60,8 +60,4 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
-    public void loginFunction(View view) {
-
-    }
 }
--- app/src/main/java/com/cs407/journeydoodle/activity_login.java
+++ app/src/main/java/com/cs407/journeydoodle/activity_login.java
@@ -2,9 +2,27 @@ package com.cs407.journeydoodle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class activity_login extends AppCompatActivity {
+    public void loginFunction(View view) {
+        EditText username = (EditText) findViewById(R.id.username);
+        String userName = username.getText().toString();
+        SharedPreferences sharedPreferences = getSharedPreferences("lab5_milstone", Context.MODE_PRIVATE);
+        sharedPreferences.edit().putString("username", userName).apply();
+        goToActivity(username.getText().toString());
+    }
+
+    public void goToActivity(String s) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("message", s);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
